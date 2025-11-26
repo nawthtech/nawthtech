d2e0c3f95dbf3b91b5a26daecccf170ac32afa87
nawthtech <limb-unbent-busily@duck.com>
2025-11-26 19:29:56 +0400

    Create cart_service.go

---
+++ backend/internal/services/cart_service.go
@@ -0,0 +1,9 @@
+// internal/services/cart_service.go
+package services
+
+// CartService يقدم خدمات إدارة السلة
+type CartService struct{}
+
+func NewCartService() *CartService {
+	return &CartService{}
+}
