78818179604cdfc2d220cbcff72dccc024a69076
nawthtech <limb-unbent-busily@duck.com>
2025-11-28 08:01:39 +0400

    Delete backend/internal/services/store_service.go

--- backend/internal/services/store_service.go
+++
@@ -1,9 +0,0 @@
-// internal/services/store_service.go
-package services
-
-// StoreService يقدم خدمات إدارة المتجر
-type StoreService struct{}
-
-func NewStoreService() *StoreService {
-	return &StoreService{}
-}
