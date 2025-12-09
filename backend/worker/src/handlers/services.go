bb363863106cb6ae3e85899561482513cf450991
nawthtech <limb-unbent-busily@duck.com>
2025-12-09 09:33:05 +0400

    Update services.go

--- backend/worker/src/handlers/services.go
+++ backend/worker/src/handlers/services.go
@@ -1,24 +1,42 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
-	"nawthtech/worker/src/utils"
+	"nawthtech/utils"
 )
 
 func GetServices(w http.ResponseWriter, r *http.Request) {
-	utils.JSONResponse(w, http.StatusOK, map[string]interface{}{
+	db := utils.GetD1DB()
+	services, _ := utils.GetAllServices(db)
+	respondJSON(w, http.StatusOK, map[string]interface{}{
 		"success": true,
-		"data":    []string{},
+		"data":    services,
 	})
 }
 
 func GetServiceByID(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	utils.JSONResponse(w, http.StatusOK, map[string]interface{}{
+	db := utils.GetD1DB()
+	id := r.URL.Path[len("/services/"):]
+	service, err := utils.GetServiceByID(db, id)
+	if err != nil {
+		respondJSON(w, http.StatusNotFound, map[string]interface{}{
+			"success": false,
+			"error":   "SERVICE_NOT_FOUND",
+		})
+		return
+	}
+
+	respondJSON(w, http.StatusOK, map[string]interface{}{
 		"success": true,
-		"data": map[string]string{
-			"id": id,
-		},
+		"data":    service,
 	})
+}
+
+// ================= Helper
+func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(payload)
 }
